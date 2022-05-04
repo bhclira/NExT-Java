63c89a2df9ab50fa9c82773239eaf5e3ea81bd6b
brenomanDEV <bhcl@cesar.school>
2022-05-03 22:07:08 -0300

    Create aula2-java-concepts

--- aula1/src/App.java
+++ aula1/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int numeroInteiro = 1
+
+        // if (numeroInteiro==1)System.out.println("aaa");
+        if (numeroInteiro==1){
+            System.out.println("Entrou na COndição");
+            numeroInteiro++;
+        }else if(numeroInteiro==2){
+            System.out.println("entrou no senao");
+            numeroInteiro++;
+            
+        }
     }
+}
+
+{
+    int n;
+    int k;
+    k = 0;
 }
---
+++ aula1/src/aula2.java
@@ -0,0 +1,43 @@
+/**
+    * @author: Breno
+    * @version: 1.0.1
+    * @todo: Print Hello World
+*/
+
+public class aula2 {
+    public static void main(String[] args) throws Exception {
+        // decla de variáveis - primitivos
+        int numeroInteiro = 8; // o bits dentro da memoria
+        float numeroFlutuante = 2.1f;
+        double numeroDouble = 2.1231; //usual
+        char caracter = 'L';
+        // type_variable nomeVariavel;
+        String str = "Luiz Francisco - Prof";
+        // atribuição dados var
+        numeroInteiro = 45;
+        
+        // operadores aritméticos
+        // soma = 1 + 1;
+        // '+ - / %'''
+        // resto = 1%1
+
+        String mensagem = "valores numero inteiro" + numeroInteiro+"numero flutuante"+numeroFlutuante;
+
+        System.out.println(mensagem);
+
+        // operadores logicos
+        System.out.println(1==1);
+        // == , > e <, >= e <=, !=
+        // && (and)
+        // || (or)
+        // !(not)
+
+
+        System.out.println("Hello, World!");
+
+    }
+    
+
+    // java doc padrão - antes da classe
+    
+}
